5a470970c331b9650195996fb436aa7623cfcfef
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-04-07 12:45:56 -0300

    tentativa de somar os elementos da matriz

--- exercicio08/exercicio08.js
+++ exercicio08/exercicio08.js
@@ -6,6 +6,11 @@ function exercicio08(){
             num[x, y] = parseFloat(prompt("Digite o " + [y+1] + " numero da " + [x+1] + " coluna"));
         }
     }
-   
+   for (x = 0; x < 2; x++){
+        for (y = 0; y < 3; y++){
+            let soma = num[x]+num[y];
+        }
+   } 
+   alert("A soma desses elementos é de: " + soma)
     
 }
